3e3c6e594c8d716ed7312b5f62fb4d9a941a7aaf
Spooky2003 <kenlobos2@icloud.com>
2025-05-10 12:44:27 +0800

    updates

--- Program.cs
+++ Program.cs
@@ -24,11 +24,9 @@
 
         SeedData.Initialize(services);
     }
-    // Configure the HTTP request pipeline.
     if (!app.Environment.IsDevelopment())
     {
         app.UseExceptionHandler("/Home/Error");
-        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
         app.UseHsts();
     }
 
